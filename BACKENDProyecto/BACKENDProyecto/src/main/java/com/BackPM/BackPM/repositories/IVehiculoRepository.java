52f50b8262cd4988bc78d251aec94ddb518ca1b9
seam10 <syong-2023a@corhuila.edu.co>
2025-05-19 19:55:21 -0500

    project

--- BACKENDProyecto/BACKENDProyecto/src/main/java/com/BackPM/BackPM/repositories/IVehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BackPM.BackPM.repositories;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IVehiculoRepository extends IBaseRepository<Vehiculo, Long>{
-    Vehiculo findByPlaca(String placa);
-}
-
