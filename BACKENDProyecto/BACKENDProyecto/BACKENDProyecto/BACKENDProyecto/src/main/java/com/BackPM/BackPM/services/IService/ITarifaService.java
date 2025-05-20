89f60ca3fc0f636bf6a92f105d4afc204a7bd3be
seam10 <syong-2023a@corhuila.edu.co>
2025-05-19 19:50:30 -0500

    project

--- BACKENDProyecto/BACKENDProyecto/BACKENDProyecto/BACKENDProyecto/src/main/java/com/BackPM/BackPM/services/IService/ITarifaService.java
+++
@@ -1,9 +0,0 @@
-package com.BackPM.BackPM.services.IService;
-
-
-import com.BackPM.BackPM.models.Tarifa;
-
-public interface ITarifaService extends IBaseService<Tarifa> {
-    Tarifa obtenerPorTipoVehiculo(String tipoVehiculo);
-}
-
