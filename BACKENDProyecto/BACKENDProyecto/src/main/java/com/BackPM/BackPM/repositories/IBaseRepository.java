bbc56ad27598f074f8692acb23d9497bb342061c
seam10 <syong-2023a@corhuila.edu.co>
2025-05-22 10:23:15 -0500

    project

--- BACKENDProyecto/BACKENDProyecto/src/main/java/com/BackPM/BackPM/repositories/IBaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.BackPM.BackPM.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BackPM.BackPM.models.ABaseEntity;
-
-public interface IBaseRepository<T extends ABaseEntity, ID> extends JpaRepository<T,ID> {
-}
